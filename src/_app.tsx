d7c341397987c008614fbbc82edf841afc3f21d9
Ojore Chris <ojorechris@gmail.com>
2023-04-22 20:15:06 +0100

    rtk-setup

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,13 +1,8 @@
-import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '@/app/store';
+import type { AppProps } from "next/app";
+import { store } from "@/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
